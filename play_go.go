afbc2705950aed822c893d8e55c7ff7973ed42ba
Suman <desuman18@gmail.com>
2021-02-14 11:16:54 +0530

    first commit

---
+++ play_go.go
@@ -0,0 +1,90 @@
+package main
+
+func main() {
+	// fmt.Println("Hello world !")
+	// // my_numbers := [10] int {}  static
+	// my_numbers := [] int {}
+
+	// for i, index := 0, 0 ; i < 10; i++ {
+	// 	if(i%2 == 0){
+	// 		my_numbers = append(my_numbers, i)
+	// 		// my_numbers[index] = i static
+	// 		index++
+	// 	}
+
+	// }
+
+	// for _, v := range my_numbers {
+	// 	fmt.Println(v)
+	// }
+
+	// fibo_numbers := [] int {0, 1}
+	// max_num := 200
+
+	// for i := 2; i < max_num; i++ {
+	// 	fibo_numbers = append(fibo_numbers, fibo_numbers[i-1] + fibo_numbers[i-2])
+	// }
+
+	// for _, v := range fibo_numbers {
+	// 	fmt.Println(v)
+	// }
+
+	// nums := [...]int{1, 3, 4, 5, 6, 8, 9}
+
+	// for index, v := range nums {
+	// 	fmt.Println(index, v)
+	// }
+
+	// type Info struct {
+	// 	name  string
+	// 	roll  int
+	// 	marks float32
+	// }
+
+	// stu1 := new(Info)
+
+	// stu1.name = "Suman De"
+	// stu1.roll = 2
+	// stu1.marks = 8.96
+
+	// fmt.Printf("Name = %s Roll = %d Marks = %.2f ", stu1.name, stu1.roll, stu1.marks)
+
+	// 	person_1 := new(Person)
+	// 	person_1.name = "Suman De"
+	// 	person_1.age = 23
+	// 	person_1.height = 5.8
+	// 	names := [...]string{"Anime", "Music", "Code"}
+
+	// 	for i := 0; i < 3; i++ {
+	// 		person_1.hobbies = append(person_1.hobbies, names[i])
+	// 	}
+
+	// 	person_1.ShowPerson()
+
+	// var name string
+	// name = "Suman"
+
+	// OR
+	// var name = "Suman"
+
+	// OR
+	// name := "Suman"
+
+	// fmt.Println(name)
+
+}
+
+// type Person struct {
+// 	name    string
+// 	age     int
+// 	height  float32
+// 	hobbies []string
+// }
+
+// func (obj Person) ShowPerson() {
+// 	fmt.Printf("Name = %s Age = %d Height = %.2f feet", obj.name, obj.age, obj.height)
+// 	fmt.Print(" Hobbies =")
+// 	for index, v := range obj.hobbies {
+// 		fmt.Printf(" %d. %s ", index+1, v)
+// 	}
+// }
